3b990af29058d24354a755856885468b52a3890d
Meritj <78165055+Meritj@users.noreply.github.com>
2021-10-13 16:03:11 +0200

    Add files via upload

---
+++ motdepasseIndex.js
@@ -0,0 +1,43 @@
+const dataLowerCase = "azertyuiopqsdfghjklmwxcvbn";
+const dataUpperCase = dataLowerCase.toUpperCase(); // ici on met en majuscule
+const dataNumber = "1234567890";
+const dataSymbol = "$*€£%&§!()-_``éè";
+const rangeValue = document.getElementById('password-length'); // ici on récupère le range grâce à l'ID
+const passwordOutput = document.getElementById('password-output');
+
+const generatePassword = () => {
+    let data = [];
+    let password = "";
+    if (lowercase.checked) {
+        data.push(...dataLowerCase)
+    }
+    if (uppercase.checked) {
+        data.push(...dataUpperCase)
+    }
+    if (number.checked) {
+        data.push(...dataNumber)
+    }
+    if (symbols.checked) {
+        data.push(...dataSymbol)
+    }
+    if (data.length === 0){
+        alert("Veuillez sélectionner des critères");
+        return;
+    }
+    for(i = 0; i < rangeValue.value; i ++){
+        password += data[Math.floor(Math.random() * data.length)];
+        console.log(password)
+    }
+    passwordOutput.value= password;
+
+    passwordOutput.select();
+    document.execCommand("copy");
+
+    generateButton.textContent = "Copié";
+
+    setTimeout(()=>{
+        generateButton.textContent="Générer un MDP";
+    },2000);
+}
+
+generateButton.addEventListener('click', generatePassword);
